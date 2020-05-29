64435496e476a11d4e9d9bd320ba7c6807a1803b
Richard Spindler <richard@lateralblast.com.au>
2020-05-29 13:56:09 +1000

    Added SCP functionality

--- README.md
+++ README.md
@@ -9,13 +9,16 @@ Introduction
 ------------
 
 New versions of SSH no longer have old ciphers compiled in as they are considered insecure.
+Similarly a lot of older equipment used 768 bit host keys and later versions of SSH will not
+connect to hosts with less that 1024 bit host keys.
+
 However, sometimes we need to connect to an old piece of hardware that has older versions 
 of SSH that can not be upgraded.
 
 This script provides a wrapper that:
 
 - Creates a docker image based on Ubuntu 16.04 which has a version of SSH with older ciphers
-- Runs SSH from the docker container with older cipher options
+- Runs SSH/SCP from the docker container with older cipher options
 
 License
 -------
@@ -49,11 +52,23 @@ SSH to a host:
 ./ostrich.sh -u admin -s 192.168.10.250
 ```
 
+SCP a file to a host:
+
+```
+./ostrich.sh /tmp/blah admin@192.168.1.199:/tmp/blah
+```
+
+SCP a file to a host:
+
+```
+./ostrich.sh -u admin -s 192.168.1.100 -c /tmp/blah -d /tmp/blah
+```
+
 Get usage information:
 
 ```
 ./ostrich.sh -h
-ostrich (Old SSH Terminal Remote Interactive Console Helper) 0.0.1
+ostrich (Old SSH Terminal Remote Interactive Console Helper) 0.0.2
 Richard Spindler <richard@lateralblast.com.au>
 
 Usage Information:
@@ -62,6 +77,10 @@ Usage Information:
          Display help
       V)
          Display Version
+      c)
+         Source file to copy (SCP)
+      d)
+         Destination file (SCP)
       v)
          Verbose mode
       u)
@@ -69,5 +88,5 @@ Usage Information:
       s)
          Hostname
       o)
-         SSH Option
+         SSH/SCP Option
 ```
--- ostrich.sh
+++ ostrich.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Name:         ostrich (Old SSH Terminal Remote Interactive Console Helper)
-# Version:      0.0.1
+# Version:      0.0.2
 # Release:      1
 # License:      CC-BA (Creative Commons By Attribution)
 #               http://creativecommons.org/licenses/by/4.0/legalcode
@@ -25,11 +25,14 @@ name="ostrich"
 
 args=$@
 
+# Default SSH / SCP options
+
+opts="-oKexAlgorithms=+diffie-hellman-group1-sha1 -oStrictHostKeyChecking=no"
+
 # Get the script info from the script itself
 
 app_vers=$(grep "^# Version" "$0" |awk '{print $3}')
 app_name=$(grep "^# Name" "$0" |awk '{for (i=3;i<=NF;++i) printf $i" "}' |sed 's/ $//g')
-app_same=$(grep "^# Name" "$0" |awk '{print $3}')
 app_pkgr=$(grep "^# Packager" "$0" |awk '{for (i=3;i<=NF;++i) printf $i" "}')
 app_help=$(grep -A1 " [A-Z,a-z])$" "$0" |sed "s/[#,\-\-]//g" |sed '/^\s*$/d')
 
@@ -53,18 +56,61 @@ print_version () {
   return
 }
 
+# Handle SCP and SSH
+
+use_ssh="true"
+use_scp="false"
+
 # If given no command line arguments print usage information
 # Handle when give @ in first argument
 
 if [ `expr "$args" : "\-"` != 1 ]; then
   if [ "$1" ]; then
-    test=$(echo "$1" |grep "@")
-    if [ "$test" ]; then
-      user=$(echo "$1" |cut -f1 -d@)
-      host=$(echo "$1" |cut -f2 -d@)
+    if [ "$2" ]; then
+      if [ "$2" == "--verbose" ]; then
+        test=$(echo "$1" |grep "@")
+        if [ "$test" ]; then
+          user=$(echo "$1" |cut -f1 -d@)
+          host=$(echo "$1" |cut -f2 -d@)
+        else
+          user=$(whoami)
+          host=$1
+        fi
+        user=$(whoami)
+        host=$1
+      else
+        test=$(echo "$2" |grep ":")
+        if [ "$test" ]; then
+          use_ssh="false"
+          use_scp="true"
+          src_file=$1
+          details=$(echo "$2" |cut -f1 -d: |tr -d '\r')
+          dst_file=$(echo "$2" |cut -f2 -d:)
+          if [ "$dst_file" = "" ]; then
+            dst_file=$src_file
+          fi
+          test=$(echo "$details" |grep "@")
+          if [ "$test" ]; then
+            user=$(echo "$details" |cut -f1 -d@)
+            host=$(echo "$details" |cut -f2 -d@)
+          else
+            user=$(whoami)
+            host=$details
+          fi
+        else
+          echo "No destination file specified"
+          exit
+        fi
+      fi
     else
-      user=$(whoami)
-      host=$1
+      test=$(echo "$1" |grep "@")
+      if [ "$test" ]; then
+        user=$(echo "$1" |cut -f1 -d@)
+        host=$(echo "$1" |cut -f2 -d@)
+      else
+        user=$(whoami)
+        host=$1
+      fi
     fi
   else
     print_help
@@ -88,14 +134,14 @@ fi
 
 # Handle --verbose
 
-if [ "$1" = "--verbose" ] || [ "$2" = "--verbose" ]; then
+if [ "$2" = "--verbose" ] || [ "$3" = "--verbose" ]; then
   verbose="true"
 fi
 
 # Handle arguments
 
 if [ ! "$user" ]; then
-  while getopts ":hvVu:s:o:" args ; do
+  while getopts ":hvVu:s:o:c:d:" args ; do
     case $args in
       h)
         # Display help
@@ -106,6 +152,16 @@ if [ ! "$user" ]; then
         # Display Version
         print_version
         exit
+        ;;
+      c)
+        # Source file to copy (SCP)
+        use_scp="true"
+        use_ssh="false"
+        src_file=$OPTARG
+        ;;
+      d)
+        # Destination file (SCP)
+        dst_file=$OPTARG
         ;;
       v)
         # Verbose mode
@@ -120,8 +176,8 @@ if [ ! "$user" ]; then
         host=$OPTARG
         ;;
       o)
-        # SSH Option
-        opts=$OPTARG
+        # SSH/SCP Option
+        new_opts=$OPTARG
         ;;
       *)
         # Display help
@@ -156,16 +212,30 @@ fi
 
 # Handle options
 
-if [ "$opts" ]; then
-  opts="-o KexAlgorithms=diffie-hellman-group1-sha1 -o StrictHostKeyChecking=no -o $opts"
-else
-  opts="-o KexAlgorithms=diffie-hellman-group1-sha1 -o StrictHostKeyChecking=no"
+if [ "$new_opts" ]; then
+  opts=$new_opts
+fi
+
+# Strip directory name from file mapped into docker
+
+if [ "$use_scp" = "true" ]; then
+  map_file=$(basename -- $src_file)
 fi
 
 # Call docker
 
 if [ "$verbose" = "true" ]; then
-  echo "Executing: docker run -it $name /bin/bash -c \"ssh $opts $user@$host\"" 
+  if [ "$use_scp" = "true" ]; then
+    echo "Executing: docker run -v $src_file:/tmp/$map_file -it $name /bin/bash -c \"scp $opts /tmp/$map_file $user@$host:$dst_file\""
+  else
+    echo "Executing: docker run -it $name /bin/bash -c \"ssh $opts $user@$host\"" 
+  fi
 fi
 
-docker run -it $name /bin/bash -c "ssh $opts $user@$host"
+# Run command SSH/SCP inside docker container to connect to host
+
+if [ "$use_scp" = "true" ]; then
+  docker run -v $src_file:/tmp/$map_file -it $name /bin/bash -c "scp $opts /tmp/$map_file $user@$host:$dst_file"
+else
+  docker run -it $name /bin/bash -c "ssh $opts $user@$host"
+fi
